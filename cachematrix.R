c6ffaf39a080dfeccd0e2640e5de3ff93c4d7926
I324826 <mihir.chittora@sap.com>
2017-08-14 19:41:31 +0530

    Assignment 2 Coursera Data Science

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These file will do the Caching of Inverse of a Matrix
 
-## Write a short comment describing this function
+## This function will crate and cache the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+          x<<-y
+          m<<-NULL
+        }
+        get<-function() x
+        setInvMatrix<-function(invMatrix){
+          m<<-invMatrix
+        }
+        getInvMatrix<-function()m
+        list(set = set, get = get,
+             setInvMatrix = setInvMatrix,
+             getInvMatrix = getInvMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function will return a new/cached inverse matrix of the passed matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<-x$getInvMatrix()
+        if(!is.null(m)){
+          message("getting cached data")
+          return(m)
+        }
+        data<-x$get()
+        m<-solve(data,...)
+        x$setInvMatrix(m)
+        m
 }
